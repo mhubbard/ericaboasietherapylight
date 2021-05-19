2f7962aec9ceff87d9112b0bbdbf7451ab523100
Mathieu Hubbard <mathieuhubbard@gmail.com>
2021-05-18 21:00:37 -0400

    more cleaning up

--- js/all.js
+++ js/all.js
@@ -37,7 +37,6 @@
         init_classic_menu();
         init_fullscreen_menu();
         init_side_panel();
-        init_lightbox();
         init_parallax();
         init_shortcodes();
         init_tooltips();
@@ -445,46 +444,6 @@
         });
         
     }
-    
-    
-    
-    /* ---------------------------------------------
-     Lightboxes
-     --------------------------------------------- */
-    
-    function init_lightbox(){
-    
-        // Works Item Lightbox				
-        $(".work-lightbox-link").magnificPopup({
-            gallery: {
-                enabled: true
-            },
-            mainClass: "mfp-fade"
-        });
-        
-        // Works Item Lightbox	
-        $(".lightbox-gallery-1").magnificPopup({
-            gallery: {
-                enabled: true
-            }
-        });
-        
-        // Other Custom Lightbox
-        $(".lightbox-gallery-2").magnificPopup({
-            gallery: {
-                enabled: true
-            }
-        });
-        $(".lightbox-gallery-3").magnificPopup({
-            gallery: {
-                enabled: true
-            }
-        });
-        $(".lightbox").magnificPopup();
-        
-    }
-    
-    
     
     /* -------------------------------------------
      Parallax
